fd66b2e0d1dafb03f533774d7d6d5b8064763d9f
atrowley <atrowley81@gmail.com>
2023-02-18 21:11:27 +0000

    Q1 (initial code)

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,10 +1,19 @@
 package reflection;
 
+import java.lang.reflect.Modifier;
+
 public class DescribeClass {
     public static void main(String[] args) {
         if (args.length != 1)
             System.out.println("Usage: "); // TODO: describe how to use the utility
-
+            System.out.println(args[0]);
+        try {
+            Class<?> myClass = Class.forName(args[0]);
+            System.out.println(myClass.getName());
+            System.out.println(Modifier.toString(myClass.getModifiers()));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         // TODO: implement the functionality for Question 1
     }
 }
